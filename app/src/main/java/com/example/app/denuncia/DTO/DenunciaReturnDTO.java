9a0ff1c9155da1cd2f46b50b7549ceaed87498ce
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-05-31 11:19:53 -0300

    retuter

--- app/src/main/java/com/example/app/denuncia/DTO/DenunciaReturnDTO.java
+++ app/src/main/java/com/example/app/denuncia/DTO/DenunciaReturnDTO.java
@@ -1,5 +1,26 @@
 package com.example.app.denuncia.DTO;
 
+import lombok.*;
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class DenunciaReturnDTO {
+    private String identifier;
     
+    private String nome;
+
+    private String dataEnchente;
+
+    private String local;
+
+    private String relato;
+
+    private Double mmChovido;
+
+    private Double previsao;
+
+    private String dataDenuncia;
 }

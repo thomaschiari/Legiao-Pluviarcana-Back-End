c80c2e4c0af1bce439a46bf9b60614231d9d294a
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-06-02 09:50:40 -0300

    retirada de filtragens

--- app/src/main/java/com/example/app/denuncia/DenunciaRepository.java
+++ app/src/main/java/com/example/app/denuncia/DenunciaRepository.java
@@ -1,12 +1,7 @@
 package com.example.app.denuncia;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
-
 
 public interface DenunciaRepository extends MongoRepository<Denuncia, String> {
     Denuncia findByIdentifier(String identifier);
-    List<Denuncia> findByDataEnchente(String dataEnchente);
-    List<Denuncia> findByDataDenuncia(String dataDenuncia);
 }

5245c8d3d3b9f4ddfc6b047d2a4b8befe2cefaf6
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-06-02 09:00:36 -0300

    merge

--- app/src/main/java/com/example/app/denuncia/DenunciaController.java
+++ app/src/main/java/com/example/app/denuncia/DenunciaController.java
@@ -1,15 +1,11 @@
 package com.example.app.denuncia;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-=======
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> ecd7d8ab540f79f8083a588ec999f0e814d10727
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;

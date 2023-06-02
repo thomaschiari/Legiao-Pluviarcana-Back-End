ec5e738a854e1a6a920e8ef770cc0e3f447a79b9
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-06-02 08:59:36 -0300

    controller iniciado

---
+++ app/src/main/java/com/example/app/denuncia/DenunciaController.java
@@ -0,0 +1,35 @@
+package com.example.app.denuncia;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.app.denuncia.DTO.*;
+
+@RestController
+@RequestMapping("/denuncia")
+public class DenunciaController {
+    @Autowired
+    DenunciaService denunciaService;
+
+    @GetMapping
+    public List<DenunciaReturnDTO> listAll(){return denunciaService.listAll();}
+
+    @PostMapping
+    public DenunciaReturnDTO saveDenuncia(@RequestBody DenunciaSaveDTO d){
+        return denunciaService.saveDenuncia(d);
+    }
+
+    @GetMapping("/{identifier}")
+    public DenunciaReturnDTO getDenuncia(@PathVariable String identifier){return denunciaService.getDenuncia(identifier);}
+
+    @DeleteMapping("/delete/{identifier}")
+    public boolean deleteDenuncia(@PathVariable String identifier){return denunciaService.deleteDenuncia(identifier);} 
+}

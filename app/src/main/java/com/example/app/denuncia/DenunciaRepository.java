1919292127e9b43ee06a521fe3ba439c69c77717
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-06-05 09:02:45 -0300

    mudança para JPA

--- app/src/main/java/com/example/app/denuncia/DenunciaRepository.java
+++ app/src/main/java/com/example/app/denuncia/DenunciaRepository.java
@@ -1,12 +1,11 @@
 package com.example.app.denuncia;
 
 
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "denuncia", path = "denuncias")
-public interface DenunciaRepository extends PagingAndSortingRepository<Denuncia, String> {
+public interface DenunciaRepository extends JpaRepository<Denuncia, String> {
 
-    Denuncia findByIdentifier(@Param("identfier") Long id);
+    Denuncia findByIdentifier(String identifier);
 }

069db5413974bee5d1b0f8b1551925e0b285e278
alfredjynx <99195331+alfredjynx@users.noreply.github.com>
2023-06-05 09:31:38 -0300

    retirada do que não é necessário

--- app/src/main/java/com/example/app/LegiaoPluviarcanaApplication.java
+++ app/src/main/java/com/example/app/LegiaoPluviarcanaApplication.java
@@ -1,13 +1,11 @@
 package com.example.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.example.app.denuncia.DenunciaRepository;
 
 @SpringBootApplication
 @EntityScan(basePackages = {
@@ -17,9 +15,6 @@ import com.example.app.denuncia.DenunciaRepository;
             "com.example.app.denuncia"
             })
 public class LegiaoPluviarcanaApplication{
-
-	@Autowired
-	DenunciaRepository denunciaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LegiaoPluviarcanaApplication.class, args);
